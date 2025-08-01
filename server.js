b8db52bc4cd2c6ff15244aadab366a387b08ce30
Thiago Mota <thiago.you23@gmail.com>
2025-08-01 16:22:22 -0300

    Adicionei o  middleware ao projeto

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import publicRoutes from "./routes/public.js";
 import privateRoutes from "./routes/private.js";
+import auth from "./middlewares/auth.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/", publicRoutes);
-app.use("/", privateRoutes);
+app.use("/", auth, privateRoutes);
 
 // motcha
 // 2veUcB3hMHxCus3f
